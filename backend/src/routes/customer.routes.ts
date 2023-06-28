8eeff9b628e4b11ce6786074ced321793503e9e2
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-06-28 15:35:13 -0300

    Put the middleware on the customer routes

--- backend/src/routes/customer.routes.ts
+++ backend/src/routes/customer.routes.ts
@@ -4,6 +4,7 @@ import { GetAllCustomersController } from "../modules/customers/useCases/getAllC
 import { GetCustomerByIdController } from "../modules/customers/useCases/getCustomerById/GetCustomerByIdController";
 import { UpdateCustomerController } from "./../modules/customers/useCases/updateCustomer/UpdateCustomerController";
 import { DeleteCustomerController } from "./../modules/customers/useCases/deleteCustomer/DeleteCustomerController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createCustomerController = new CreateCustomerController();
 const getAllCustomersController = new GetAllCustomersController();
@@ -13,10 +14,22 @@ const deleteCustomerController = new DeleteCustomerController();
 
 const customerRoutes = Router();
 
-customerRoutes.post("/", createCustomerController.handle);
-customerRoutes.get("/", getAllCustomersController.handle);
-customerRoutes.get("/:id", getCustomerByIdController.handle);
-customerRoutes.put("/:id", updateCustomerController.handle);
-customerRoutes.delete("/:id", deleteCustomerController.handle);
+customerRoutes.post("/", ensureAuthenticated, createCustomerController.handle);
+customerRoutes.get("/", ensureAuthenticated, getAllCustomersController.handle);
+customerRoutes.get(
+  "/:id",
+  ensureAuthenticated,
+  getCustomerByIdController.handle
+);
+customerRoutes.put(
+  "/:id",
+  ensureAuthenticated,
+  updateCustomerController.handle
+);
+customerRoutes.delete(
+  "/:id",
+  ensureAuthenticated,
+  deleteCustomerController.handle
+);
 
 export { customerRoutes };
