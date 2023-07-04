780288d0fe3889dd8663b3ac31ebd5c63a668505
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-04 15:25:52 -0300

    Added swagger and documented user routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { userRoutes } from "./user.routes";
 import { customerRoutes } from "./customer.routes";
 import { invoiceRoutes } from "./invoice.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../../swagger.json";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/customers", customerRoutes);
 routes.use("/invoices", invoiceRoutes);
+routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { routes };
