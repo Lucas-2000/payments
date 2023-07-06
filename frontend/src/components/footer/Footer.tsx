cff6fe3ebdb0ee3ca089844b1f8af96cad1941b3
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-06 12:33:51 -0300

    Removed frontend

--- frontend/src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <div className="flex flex-wrap gap-4 items-center justify-center w-full border-t-4 p-2 mt-8">
-      <p className="text-slate-400">© 2023 Payments Remember</p>
-    </div>
-  );
-};
